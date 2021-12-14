0d3a03ec22179c911e25ab86e3155d4e31f8126f
Bala Raman <1273137+srbala@users.noreply.github.com>
2021-12-14 16:13:34 -0500

    Adding ARM64 and script fixes (#15)

--- WSL-DistroLauncher/DistroLauncher/DistributionInfo.cpp
+++ WSL-DistroLauncher/DistroLauncher/DistributionInfo.cpp
@@ -12,25 +12,22 @@ bool DistributionInfo::CreateUser(std::wstring_view userName)
     std::wstring commandLine = L"/usr/sbin/useradd -m ";
     commandLine += userName;
     HRESULT hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
-    if ((FAILED(hr)) || (exitCode != 0))
-    {
+    if ((FAILED(hr)) || (exitCode != 0)) {
         return false;
     }
 
     commandLine = L"/usr/bin/passwd ";
     commandLine += userName;
     hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
-    if ((FAILED(hr)) || (exitCode != 0))
-    {
+    if ((FAILED(hr)) || (exitCode != 0)) {
         return false;
     }
 
     // Add the user account to any relevant groups.
-    commandLine = L"/usr/sbin/usermod -aG wheel ";
+    commandLine = L"/usr/sbin/usermod -aG wheel,adm,cdrom ";
     commandLine += userName;
     hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
-    if ((FAILED(hr)) || (exitCode != 0))
-    {
+    if ((FAILED(hr)) || (exitCode != 0)) {
 
         // Delete the user if the group add command failed.
         commandLine = L"/usr/sbin/userdel -r ";
@@ -43,8 +40,7 @@ bool DistributionInfo::CreateUser(std::wstring_view userName)
     commandLine += userName;
     commandLine += L" ALL=(ALL) NOPASSWD:ALL\n' > /etc/sudoers.d/default_user";
     hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
-    if ((FAILED(hr)) || (exitCode != 0))
-    {
+    if ((FAILED(hr)) || (exitCode != 0)) {
         return false;
     }
 
@@ -58,41 +54,32 @@ ULONG DistributionInfo::QueryUid(std::wstring_view userName)
     HANDLE writePipe;
     SECURITY_ATTRIBUTES sa{sizeof(sa), nullptr, true};
     ULONG uid = UID_INVALID;
-    if (CreatePipe(&readPipe, &writePipe, &sa, 0))
-    {
+    if (CreatePipe(&readPipe, &writePipe, &sa, 0)) {
         // Query the UID of the supplied username.
         std::wstring command = L"/usr/bin/id -u ";
         command += userName;
         int returnValue = 0;
         HANDLE child;
         HRESULT hr = g_wslApi.WslLaunch(command.c_str(), true, GetStdHandle(STD_INPUT_HANDLE), writePipe, GetStdHandle(STD_ERROR_HANDLE), &child);
-        if (SUCCEEDED(hr))
-        {
+        if (SUCCEEDED(hr)) {
             // Wait for the child to exit and ensure process exited successfully.
             WaitForSingleObject(child, INFINITE);
             DWORD exitCode;
-            if ((GetExitCodeProcess(child, &exitCode) == false) || (exitCode != 0))
-            {
+            if ((GetExitCodeProcess(child, &exitCode) == false) || (exitCode != 0)) {
                 hr = E_INVALIDARG;
             }
 
             CloseHandle(child);
-            if (SUCCEEDED(hr))
-            {
+            if (SUCCEEDED(hr)) {
                 char buffer[64];
                 DWORD bytesRead;
 
                 // Read the output of the command from the pipe and convert to a UID.
-                if (ReadFile(readPipe, buffer, (sizeof(buffer) - 1), &bytesRead, nullptr))
-                {
+                if (ReadFile(readPipe, buffer, (sizeof(buffer) - 1), &bytesRead, nullptr)) {
                     buffer[bytesRead] = ANSI_NULL;
-                    try
-                    {
+                    try {
                         uid = std::stoul(buffer, nullptr, 10);
-                    }
-                    catch (...)
-                    {
-                    }
+                    } catch( ... ) { }
                 }
             }
         }
